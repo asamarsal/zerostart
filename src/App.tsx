d2710fc15a81a37dff133dca61add25d968ef26a
asamarsal <ASAMARSAL@gmail.com>
2025-07-26 17:44:23 +0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+import { ConnectButton } from '@xellar/kit';
+import { erc20Abi, formatUnits, parseEther } from 'viem';
+import {
+  useAccount,
+  useReadContract,
+  useSignMessage,
+  useSendTransaction,
+  useChainId,
+} from 'wagmi';
+
+function App() {
+  const { address } = useAccount();
+
+  const chainId = useChainId();
+
+  const {
+    signMessage,
+    data: signedMessage,
+    isPending: isSigning,
+  } = useSignMessage();
+
+  const { data, isLoading: isReading } = useReadContract({
+    abi: erc20Abi,
+    account: address,
+    address: '0x3eede3fe85f32d013e368d02db07c0662390eadd',
+    functionName: 'balanceOf',
+    args: [address as `0x${string}`],
+    chainId,
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  const {
+    sendTransactionAsync,
+    data: sentTransaction,
+    error: sendTransactionError,
+    isPending: isSending,
+  } = useSendTransaction();
+
+  const handleSendTransaction = async () => {
+    if (!address) return;
+    await sendTransactionAsync({
+      to: address as `0x${string}`,
+      value: parseEther('0'),
+      chainId,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center min-h-dvh w-screen bg-neutral-900 p-12">
+      <div className="text-center text-2xl font-bold pb-6 text-white">
+        <h2>ZeroStart</h2>
+      </div>
+
+      <ConnectButton />
+
+      {address && (
+        <>
+          <button
+            disabled={isSigning}
+            className="bg-blue-500 text-white rounded-md px-4 py-2 cursor-pointer"
+            onClick={() => signMessage({ message: 'Hello, world!' })}>
+            {isSigning ? 'Signing...' : 'Sign message'}
+          </button>
+
+          {signedMessage ? (
+            <div className="text-white max-w-full overflow-hidden">
+              <pre className="whitespace-pre-wrap break-all overflow-wrap-anywhere">
+                {JSON.stringify({ signedMessage }, null, 2)}
+              </pre>
+            </div>
+          ) : null}
+        </>
+      )}
+
+      {address && (
+        <>
+          <button
+            disabled={isSending}
+            className="bg-blue-500 text-white rounded-md px-4 py-2 cursor-pointer"
+            onClick={handleSendTransaction}>
+            {isSending ? 'Sending...' : 'Send Transaction'}
+          </button>
+
+          {sentTransaction ? (
+            <div className="text-white max-w-full overflow-hidden">
+              <p>Sent Transaction</p>
+              <pre className="whitespace-pre-wrap font-mono break-all overflow-wrap-anywhere">
+                {JSON.stringify({ sentTransaction }, null, 2)}
+              </pre>
+            </div>
+          ) : null}
+
+          {sendTransactionError ? (
+            <div className="text-white max-w-full overflow-hidden">
+              <p>Send Transaction Error</p>
+              <pre className="whitespace-pre-wrap font-mono break-all overflow-wrap-anywhere">
+                {JSON.stringify(
+                  {
+                    sendTransactionError: (sendTransactionError as any)?.cause
+                      ?.details,
+                  },
+                  null,
+                  2,
+                )}
+              </pre>
+            </div>
+          ) : null}
+        </>
+      )}
+
+      {address && !isReading ? (
+        <div className="text-white max-w-full overflow-hidden">
+          <p>Read Contract Result:</p>
+          <pre className="whitespace-pre-wrap font-mono break-all overflow-wrap-anywhere">
+            {JSON.stringify(
+              { data: data ? formatUnits(data as bigint, 6) : '0' },
+              null,
+              2,
+            )}
+          </pre>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Web3Provider.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { WagmiProvider, type Config } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { XellarKitProvider, defaultConfig, darkTheme } from '@xellar/kit';
+import { liskSepolia, sepolia } from 'viem/chains';
+
+const xellarAppId = import.meta.env.VITE_XELLAR_APP_ID;
+const walletConnectProjectId = import.meta.env.VITE_WC_PROJECT_ID;
+
+const config = defaultConfig({
+  appName: 'Xellar',
+  // Required for WalletConnect
+  walletConnectProjectId,
+  // Required for Xellar Passport
+  xellarAppId: xellarAppId,
+  xellarEnv: 'sandbox',
+  chains: [sepolia, liskSepolia],
+}) as Config;
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <XellarKitProvider theme={darkTheme}>{children}</XellarKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
